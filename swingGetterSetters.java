3da86bd6fa72bbb39daf24c5d35ac9cc02dbbbb5
Joseph Ayoma <josephayoma1@gmail.com>
2021-08-20 17:09:02 +0300

    Update swingGetterSetters.java

--- swingGetterSetters.java
+++ swingGetterSetters.java
@@ -3,7 +3,6 @@
  * To change this template file, choose Tools | Templates
  * and open the template in the editor.
  */
-package sectionsampleswinggui;
 
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
@@ -17,7 +16,7 @@ import javax.swing.table.DefaultTableModel;
 
 /**
  *
- * @author Dockman
+ * @author Joseph Ayoma
  */
 public class swingGetterSetters {
 
